60da36d3c2f94191bd044d5ca067ebfd6f2134a9
Aleksandr_Kosholap <kosholap@gmail.com>
2014-12-24 09:17:12 +0200

    12

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates_length_of :title, :minimum => 5
+  validates_length_of :title, :maximum => 100
+  validates_presence_of :title, :body
+  validates_uniqueness_of :title
 end
